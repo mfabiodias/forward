aa50c0a7060d6607d0ac540ad12780fbd974fc6b
Fabio Messias Dias <mfabiodias@gmail.com>
2022-06-20 23:04:36 -0300

    fix

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+// ok
+
 app.get('*', (req, res) => {
   console.log(req.method, req.path);
   console.log(req.headers);
   res.send();
 });
+
 app.post('*', (req, res) => {
   console.log(req.method, req.path);
   console.log(req.headers);
